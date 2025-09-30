b714a772c0f4696301ef8b4862d7974cb13b2cfa
Erdem Ocak <lorderdemm@icloud.com>
2025-09-30 22:00:32 +0300

    Create types_yunus_erdem_ocak.py

---
+++ Week02/types/types_yunus_erdem_ocak.py
@@ -0,0 +1,16 @@
+# An integer with the name: my_int
+my_int = 5
+
+# A float with the name: my_float
+my_float = 5.8
+
+# A boolean with the name: my_boolean
+my_bool = True
+
+# A complex with the name: my_complex
+my_complex = 2 + 4j
+
+print(my_int)
+print(my_bool)
+print(my_float)
+print(my_complex)
