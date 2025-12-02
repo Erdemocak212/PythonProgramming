1bb8e4598ef6d0b899d099f3b4c3a2e3b0113a73
Erdem Ocak <lorderdemm@icloud.com>
2025-12-02 15:53:34 +0300

    Add function to calculate pyramid height from blocks

---
+++ Week03/pyramid_yunus_erdem_ocak.py
@@ -0,0 +1,9 @@
+def calculate_pyramid_height(number_of_blocks: int) -> int:
+    height = 0
+    used = 0
+
+    while used + (height + 1) <= number_of_blocks:
+        height += 1
+        used += height
+
+    return height
